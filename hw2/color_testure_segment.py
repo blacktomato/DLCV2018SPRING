380dd4804bbb990e2b1321d1e340bdc900204b2c
ChungSL <b02901001@ntu.edu.tw>
2018-04-04 22:27:14 +0800

    finish color & texture clustering

--- hw2/color_testure_segment.py
+++ hw2/color_testure_segment.py
@@ -4,7 +4,7 @@
  # File Name : color_testure_segment.py
  # Purpose : Color & Texture Segmentation for images
  # Creation Date : 廿十八年三月廿八日 (週三) 十五時41分十秒
- # Last Modified : 廿十八年四月四日 (週三) 廿一時51分卅一秒
+ # Last Modified : 廿十八年四月四日 (週三) 廿二時十八分一秒
  # Created By : SL Chung
 ##############################################################
 
@@ -38,10 +38,11 @@ kmeans = KMeans(n_clusters = k, max_iter = max_iteration)
 
 #Which task should be done
 Color_Cluster = False
-Texture_Cluster = True 
+Texture_Cluster = False
+Color_Texture_Cluster = True
 
 if Color_Cluster:
-    flatten_z = img_z.reshape((s_z[0]*s_z[1], s_h[2]))
+    flatten_z = img_z.reshape((s_z[0]*s_z[1], s_z[2]))
     flatten_m = img_m.reshape((s_m[0]*s_m[1], s_m[2]))
     print("KMeans clustering...")
     start_time = time.time()
@@ -85,9 +86,50 @@ if Texture_Cluster:
     #Output images
     print("Saving images...")
     start_time = time.time()
-    cluster_z_texture = kmeans.fit_predict(filtered_z).reshape((s_z[0], s_z[1]))
     img_Tcluster_z = colors[cluster_z_texture]
     cv2.imwrite("Tcluster_zebra.jpg", img_Tcluster_z)
     img_Tcluster_m = colors[cluster_m_texture]
     cv2.imwrite("Tcluster_mountain.jpg", img_Tcluster_m)
+    print("Done!")
+
+#Start the KMeans Algorithm
+k = 6
+kmeans = KMeans(n_clusters = k, max_iter = max_iteration)
+
+if Color_Texture_Cluster:
+    flatten_z = img_z.reshape((s_z[0]*s_z[1], s_z[2]))
+    flatten_m = img_m.reshape((s_m[0]*s_m[1], s_m[2]))
+
+    #Symmetric Padding
+    gray_z = cv2.cvtColor(img_z, cv2.COLOR_BGR2GRAY)
+    gray_m = cv2.cvtColor(img_m, cv2.COLOR_BGR2GRAY)
+    #Use filter to transform the image shape to h*w*38
+    filtered_z = np.zeros((s_z[0], s_z[1], F.shape[2]))
+    filtered_m = np.zeros((s_m[0], s_m[1], F.shape[2]))
+    print("Using the filter to find texture...")
+    start_time = time.time()
+    for i in range(F.shape[2]):
+        filtered_z[:,:,i] = signal.correlate2d(gray_z, F[:,:,i], boundary='symm', mode='same')
+        filtered_m[:,:,i] = signal.correlate2d(gray_m, F[:,:,i], boundary='symm', mode='same')
+    print("--- %s seconds ---" % (time.time() - start_time))
+    filtered_z = filtered_z.reshape((s_z[0]*s_z[1], F.shape[2]))
+    filtered_m = filtered_m.reshape((s_m[0]*s_m[1], F.shape[2]))
+    
+    print("Concatenate texture and color dimension...")
+    CT_z = np.concatenate( (flatten_z, filtered_z), 1)
+    CT_m = np.concatenate( (flatten_m, filtered_m), 1)
+
+    print("KMeans clustering...")
+    start_time = time.time()
+    cluster_z_ct = kmeans.fit_predict(CT_z).reshape((s_z[0], s_z[1]))
+    cluster_m_ct = kmeans.fit_predict(CT_m).reshape((s_m[0], s_m[1]))
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+    #Output images
+    print("Saving images...")
+    start_time = time.time()
+    img_CTcluster_z = colors[cluster_z_ct]
+    cv2.imwrite("CTcluster_zebra.jpg", img_CTcluster_z)
+    img_CTcluster_m = colors[cluster_m_ct]
+    cv2.imwrite("CTcluster_mountain.jpg", img_CTcluster_m)
     print("Done!")
