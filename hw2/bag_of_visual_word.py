d3b05ba24f22ebfafabcc084048f9da43ebbbf0d
ChungSL <b02901001@ntu.edu.tw>
2018-04-06 17:13:43 +0800

    SURF

--- hw2/bag_of_visual_word.py
+++ hw2/bag_of_visual_word.py
@@ -4,7 +4,7 @@
  # File Name : bag_of_visual_word.py
  # Purpose : Recognition with Bag of Visual Word
  # Creation Date : 廿十八年四月四日 (週三) 廿二時廿九分十六秒
- # Last Modified : 廿十八年四月六日 (週五) 十四時十六分廿四秒
+ # Last Modified : 廿十八年四月六日 (週五) 十七時〇分51秒
  # Created By : SL Chung
 ##############################################################
 
@@ -39,3 +39,14 @@ for cidx, category in enumerate(os.listdir(test100_path)):
     temp_path = test100_path + category + '/'
     for fidx, file in enumerate(os.listdir(temp_path)):
         test100[cidx*100+fidx] = cv2.imread(temp_path+file)
+#Choose the Train10 img and plot the interest point at most 30
+for idx in train10:
+    surf = cv2.xfeatures2d.SURF_create(500)
+    while(True):
+        kp, des = surf.detectAndCompute(train10[idx], None)
+        if len(kp) <= 30:
+            print(idx, surf.getHessianThreshold(), len(kp))
+            break
+        surf.setHessianThreshold(surf.getHessianThreshold() + 300)
+temp = cv2.drawKeypoints(train10[0],kp,None,(255,0,0),4)
+cv2.imwrite("temp.jpg", temp)
