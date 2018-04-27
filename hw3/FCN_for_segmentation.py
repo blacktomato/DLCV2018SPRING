6a7f912e0b140c9285504d86cafcada79300224a
ChungSL <b02901001@ntu.edu.tw>
2018-04-27 16:19:47 +0800

    data

--- hw3/FCN_for_segmentation.py
+++ hw3/FCN_for_segmentation.py
@@ -4,7 +4,7 @@
  # File Name : FCN_for_segmentation.py
  # Purpose : Training a Fully Convolution Network for Image Segmentation
  # Creation Date : 廿十八年四月廿五日 (週三) 十一時廿八分34秒
- # Last Modified : 2018年04月27日 (週五) 11時24分34秒
+ # Last Modified : 廿十八年四月廿七日 (週五) 十六時十五分37秒
  # Created By : SL Chung
 ##############################################################
 import sys
@@ -17,37 +17,18 @@ from keras.models import Model
 # GPU setting\n",
 import tensorflow as tf
 from keras.backend.tensorflow_backend import set_session
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.9)
+sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+set_session(sess)
 
 #sat & mask
-filepath = '../data/hw3_dataset/train/'
-sat_list = [file for file in os.listdir(filepath) if file.endswith('.jpg')]
-mask_list = [file for file in os.listdir(filepath) if file.endswith('.png')]
-sat_list.sort()
-mask_list.sort()
-n_sats   = len(sat_list)
-n_masks = len(mask_list)
 n_classes = 7
 h=512
 w=512
 d=3
-sat = np.empty((n_sats, h, w, d))
-for i, file in enumerate(sat_list):
-    sat[i] = mpimg.imread(os.path.join(filepath, file))/255
-
-mask = np.empty((n_masks, h, w, n_classes))
-cate = np.eye(n_classes)
-for i, file in enumerate(mask_list):
-    m = mpimg.imread(os.path.join(filepath, file))
-    m = (m >= 1).astype(int)
-    m = 4 * m[:, :, 0] + 2 * m[:, :, 1] + m[:, :, 2]    
-    mask[i, m == 3] =cate[0]  # (Cyan:   011) Urban land 
-    mask[i, m == 6] =cate[1]  # (Yellow: 110) Agriculture land 
-    mask[i, m == 5] =cate[2]  # (Purple: 101) Rangeland 
-    mask[i, m == 2] =cate[3]  # (Green:  010) Forest land 
-    mask[i, m == 1] =cate[4]  # (Blue:   001) Water 
-    mask[i, m == 7] =cate[5]  # (White:  111) Barren land 
-    mask[i, m == 0] =cate[6]  # (Black:  000) Unknown
-    mask[i, m == 4] =cate[6]  # (Red:    100) Unknown 
+sat = np.load('./sat.npy')
+mask = np.load('./mask.npy')
 
 img_input = Input(shape=(512,512,3))
 x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1', trainable=True)(img_input)
@@ -79,7 +60,7 @@ model.load_weights(weights_path, by_name=True)
 
 o = model.layers[-1].output
 
-o = Conv2D(4096, (7, 7), activation='relu', padding='same',kernel_initializer='he_normal')(o)
+o = Conv2D(4096, (5, 5), activation='relu', padding='same',kernel_initializer='he_normal')(o)
 o = Dropout(0.5)(o)
 o = Conv2D(4096, (1, 1), activation='relu', padding='same',kernel_initializer='he_normal')(o)
 o = Dropout(0.5)(o)
@@ -93,9 +74,9 @@ fcn_model.summary()
 fcn_model.compile(optimizer='adam',
 	loss='categorical_crossentropy',
 	metrics=['accuracy'])
-fcn_model.fit(sat, mask, epochs= 1 , batch_size= 20 )
+fcn_model.fit(sat, mask, epochs= 1 , batch_size= 15 )
 fcn_model.save('VGG_trainable_epoch1.h5')
-fcn_model.fit(sat, mask, epochs= 19, batch_size= 20 )
-fcn_model.save('VGG_trainable_epoch20.h5')
-fcn_model.fit(sat, mask, epochs= 20, batch_size= 20 )
-fcn_model.save('VGG_trainable_epoch40.h5')
+fcn_model.fit(sat, mask, epochs= 14, batch_size= 15 )
+fcn_model.save('VGG_trainable_epoch15.h5')
+fcn_model.fit(sat, mask, epochs= 15, batch_size= 15 )
+fcn_model.save('VGG_trainable_epoch30.h5')
