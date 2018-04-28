e8ccb23d59a847ddcda54e59d8eab5fa3d9e2e34
ChungSL <b02901001@ntu.edu.tw>
2018-04-28 13:51:48 +0800

    test

--- hw3/FCN_32s.py
+++ hw3/FCN_32s.py
@@ -4,7 +4,7 @@
  # File Name : FCN_32s.py
  # Purpose : Training a Fully Convolution Network for Image Segmentation 32s version
  # Creation Date : 廿十八年四月廿五日 (週三) 十一時廿八分34秒
- # Last Modified : 廿十八年四月廿七日 (週五) 十七時39分43秒
+ # Last Modified : 廿十八年四月廿八日 (週六) 十三時十三分十秒
  # Created By : SL Chung
 ##############################################################
 import sys
@@ -19,17 +19,40 @@ from keras import optimizers
 import tensorflow as tf
 from keras.backend.tensorflow_backend import set_session
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.9)
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5)
 sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 set_session(sess)
 
 #sat & mask
+filepath = '../data/hw3_dataset/train/'
+sat_list = [file for file in os.listdir(filepath) if file.endswith('.jpg')]
+mask_list = [file for file in os.listdir(filepath) if file.endswith('.png')]
+sat_list.sort()
+mask_list.sort()
+n_sats   = len(sat_list)
+n_masks = len(mask_list)
 n_classes = 7
 h=512
 w=512
 d=3
-sat = np.load('./sat.npy')
-mask = np.load('./mask.npy')
+sat = np.empty((n_sats, h, w, d))
+for i, file in enumerate(sat_list):
+    sat[i] = mpimg.imread(os.path.join(filepath, file))/255
+
+mask = np.empty((n_masks, h, w, n_classes))
+cate = np.eye(n_classes)
+for i, file in enumerate(mask_list):
+    m = mpimg.imread(os.path.join(filepath, file))
+    m = (m >= 1).astype(int)
+    m = 4 * m[:, :, 0] + 2 * m[:, :, 1] + m[:, :, 2]    
+    mask[i, m == 3] =cate[0]  # (Cyan:   011) Urban land 
+    mask[i, m == 6] =cate[1]  # (Yellow: 110) Agriculture land 
+    mask[i, m == 5] =cate[2]  # (Purple: 101) Rangeland 
+    mask[i, m == 2] =cate[3]  # (Green:  010) Forest land 
+    mask[i, m == 1] =cate[4]  # (Blue:   001) Water 
+    mask[i, m == 7] =cate[5]  # (White:  111) Barren land 
+    mask[i, m == 0] =cate[6]  # (Black:  000) Unknown
+    mask[i, m == 4] =cate[6]  # (Red:    100) Unknown 
 
 img_input = Input(shape=(512,512,3))
 x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1', trainable=True)(img_input)
--- hw3/FCN_8s.py
+++ hw3/FCN_8s.py
@@ -4,7 +4,7 @@
  # File Name : FCN_8s.py
  # Purpose : Training a Fully Convolution Network for Image Segmentation 8s version
  # Creation Date : 廿十八年四月廿五日 (週三) 十一時廿八分34秒
- # Last Modified : 廿十八年四月廿七日 (週五) 十七時39分十四秒
+ # Last Modified : 廿十八年四月廿八日 (週六) 十三時49分廿三秒
  # Created By : SL Chung
 ##############################################################
 import sys
@@ -12,24 +12,53 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-from keras.layers import Input, Conv2D, MaxPooling2D, Conv2DTranspose, UpSampling2D, Activation, Dropout
+from keras.layers import Input, Conv2D, MaxPooling2D, Conv2DTranspose, UpSampling2D, Activation, Dropout, Concatenate
 from keras.models import Model
 from keras import optimizers
 # GPU setting\n",
 import tensorflow as tf
 from keras.backend.tensorflow_backend import set_session
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.9)
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5)
 sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 set_session(sess)
 
 #sat & mask
+filepath = '../data/hw3_dataset/train/'
+sat_list = [file for file in os.listdir(filepath) if file.endswith('.jpg')]
+mask_list = [file for file in os.listdir(filepath) if file.endswith('.png')]
+sat_list.sort()
+mask_list.sort()
+#n_sats   = len(sat_list)
+#n_masks = len(mask_list)
+n_sats   = 500
+n_masks = 500 
 n_classes = 7
 h=512
 w=512
 d=3
-sat = np.load('./sat.npy')
-mask = np.load('./mask.npy')
+sat = np.empty((n_sats, h, w, d))
+for i, file in enumerate(sat_list):
+    if i == 500:
+        break
+    sat[i] = mpimg.imread(os.path.join(filepath, file))/255
+
+mask = np.empty((n_masks, h, w, n_classes))
+cate = np.eye(n_classes)
+for i, file in enumerate(mask_list):
+    if i == 500:
+        break
+    m = mpimg.imread(os.path.join(filepath, file))
+    m = (m >= 1).astype(int)
+    m = 4 * m[:, :, 0] + 2 * m[:, :, 1] + m[:, :, 2]    
+    mask[i, m == 3] =cate[0]  # (Cyan:   011) Urban land 
+    mask[i, m == 6] =cate[1]  # (Yellow: 110) Agriculture land 
+    mask[i, m == 5] =cate[2]  # (Purple: 101) Rangeland 
+    mask[i, m == 2] =cate[3]  # (Green:  010) Forest land 
+    mask[i, m == 1] =cate[4]  # (Blue:   001) Water 
+    mask[i, m == 7] =cate[5]  # (White:  111) Barren land 
+    mask[i, m == 0] =cate[6]  # (Black:  000) Unknown
+    mask[i, m == 4] =cate[6]  # (Red:    100) Unknown 
 
 img_input = Input(shape=(512,512,3))
 x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1', trainable=True)(img_input)
@@ -43,14 +72,14 @@ x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool', trainable=True)(x)
 x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1', trainable=True)(x)
 x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2', trainable=True)(x)
 x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3', trainable=True)(x)
-x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool', trainable=True)(x)
+pool3 = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool', trainable=True)(x)
 
-x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1', trainable=True)(x)
+x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1', trainable=True)(pool3)
 x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2', trainable=True)(x)
 x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3', trainable=True)(x)
-x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool', trainable=True)(x)
+pool4 = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool', trainable=True)(x)
 
-x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1', trainable=True)(x)
+x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1', trainable=True)(pool4)
 x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2', trainable=True)(x)
 x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3', trainable=True)(x)
 x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool', trainable=True)(x)
@@ -65,8 +94,16 @@ o = Conv2D(4096, (3, 3), activation='relu', padding='same',kernel_initializer='h
 o = Dropout(0.5)(o)
 o = Conv2D(4096, (1, 1), activation='relu', padding='same',kernel_initializer='he_normal')(o)
 o = Dropout(0.5)(o)
+o = Conv2DTranspose(256, kernel_size=(64, 64), strides=(4, 4), padding='same', name='FCN_convtrans1')(o)
+pool4_up = Conv2DTranspose(256, kernel_size=(64, 64), strides=(2, 2), padding='same', name='FCN_Upsampling_Pool4')(pool4)
+
+#Concatenate the Three Layers
+o = Concatenate([o, pool4_up, pool3])
+o = Dropout(0.5)(o)
+
 o = Conv2D( n_classes, (1 ,1), kernel_initializer='he_normal')(o)
-o = Conv2DTranspose(n_classes, kernel_size=(64, 64), strides=(32, 32), padding='same', name='FCN_convtrans1')(o)
+o = Conv2DTranspose(n_classes, kernel_size=(64, 64), strides=(8, 8), padding='same', name='FCN_convtrans2')(o)
+
 o = Activation('softmax')(o)
 fcn_model = Model( img_input , o )
 
@@ -77,8 +114,8 @@ fcn_model.compile(optimizer=adam,
 	loss='categorical_crossentropy',
 	metrics=['accuracy'])
 fcn_model.fit(sat, mask, epochs= 1 , batch_size= 15 )
-fcn_model.save('VGG_trainable_epoch1.h5')
+fcn_model.save('FCN_8s_epoch1.h5')
 fcn_model.fit(sat, mask, epochs= 14, batch_size= 15 )
-fcn_model.save('VGG_trainable_epoch15.h5')
+fcn_model.save('FCN_8s_epoch15.h5')
 fcn_model.fit(sat, mask, epochs= 15, batch_size= 15 )
-fcn_model.save('VGG_trainable_epoch30.h5')
+fcn_model.save('FCN_8s_epoch30.h5')
