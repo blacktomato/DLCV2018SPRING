a2af1813e19a781e6c6f523b84f96faa65f9d604
ChungSL <b02901001@ntu.edu.tw>
2018-05-18 22:26:41 +0800

    bash script for TA

---
+++ hw4/hw4.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+wget -O 'ACGAN_D_e20.pt' 'https://www.dropbox.com/s/ijs5f7cgqitxcsm/ACGAN_D_e20_.pt?dl=1'
+wget -O 'ACGAN_G_e20.pt' 'https://www.dropbox.com/s/30c19nd846aj8zr/ACGAN_G_e20_.pt?dl=1'
+wget -O 'GAN_D_e25.pt' 'https://www.dropbox.com/s/vz3belji3a0bmwd/GAN_D_e25_.pt?dl=1'
+wget -O 'GAN_G_e25.pt' 'https://www.dropbox.com/s/041v2tb8ysxudnd/GAN_G_e25_.pt?dl=1'
+wget -O 'VAE_e100_lKL1e-5.pt' 'https://www.dropbox.com/s/pardbcxuyqf8ma7/VAE_e100_lKL1e-5.pt?dl=1'
+mkdir ./log
+cd ./log
+wget -O 'log.zip' 'https://www.dropbox.com/sh/m1dbkjnl2uim85v/AADkH4PKwLuksC-m-EKRe2hoa?dl=1'
+unzip log.zip
+cd ..
+python3 VAE_test.py VAE_e100_lKL1e-5.pt $1 $2
+python3 GAN_test.py GAN_G_e25.pt
+python3 ACGAN_test.py ACGAN_G_e20.pt
+python3 read_tb_event.py ./log
