7c168c3cdba1677b9500128be8c143e266d784a2
blacktomato <s90169@gmail.com>
2018-06-02 16:05:50 +0800

    change variable name

--- hw5/ResNet_test.py
+++ hw5/ResNet_test.py
@@ -4,7 +4,7 @@
  # File Name : ResNet_test.py
  # Purpose : Test the Feature and FC for classification
  # Creation Date : 2018年05月30日 (週三) 15時55分47秒
- # Last Modified : 2018年05月31日 (週四) 19時45分18秒
+ # Last Modified : 2018年06月02日 (週六) 15時39分15秒
  # Created By : SL Chung
 ##############################################################
 import sys
@@ -35,19 +35,19 @@ if __name__=='__main__':
     n_classes = 11
     batch_size = 100
 
-    valid_info = getVideoList(sys.argv[3])
-    valid_path = sys.argv[2] 
-    valid_category = valid_info['Video_category']
-    valid_name = valid_info['Video_name']
-    valid_tag = np.array(valid_info['Action_labels']).astype('float') 
-    valid_tag = torch.from_numpy(valid_tag)
-    del valid_info
+    video_info = getVideoList(sys.argv[3])
+    video_path = sys.argv[2] 
+    video_category = video_info['Video_category']
+    video_name = video_info['Video_name']
+    video_tag = np.array(video_info['Action_labels']).astype('float') 
+    video_tag = torch.from_numpy(video_tag)
+    del video_info
 
     #Using ResNet50 in the function _ Video2Tensor
-    valid_ts = Video2Tensor(valid_path, valid_category, valid_name)
+    video_ts = Video2Tensor(video_path, video_category, video_name)
 
-    valid_set = Data.TensorDataset(valid_ts, valid_tag.long())
-    validloader = Data.DataLoader(dataset=valid_set, batch_size=batch_size) 
+    video_set = Data.TensorDataset(video_ts, video_tag.long())
+    videoloader = Data.DataLoader(dataset=video_set, batch_size=batch_size) 
 
     Net = torch.load(sys.argv[1])
     Net.cuda()
@@ -56,13 +56,13 @@ if __name__=='__main__':
     v_accuracy = 0
     
     with open(os.path.join(sys.argv[4], 'p1_valid.txt'), 'w+') as f:
-        for batch_idx, (b_feature, b_tag) in enumerate(validloader):
+        for batch_idx, (b_feature, b_tag) in enumerate(videoloader):
             result = torch.argmax(Net(b_feature.cuda()).detach(), 1).cpu().numpy()
             for i in range(len(result)):
                 f.write(str(result[i])+'\n')
             v_accuracy += torch.sum(result == b_tag)
 
-    v_accuracy = v_accuracy.float() / len(validloader.dataset) * 100
-    print('Accuracy for Validation Set:', v_accuracy.item())
+    v_accuracy = v_accuracy.float() / len(videoloader.dataset) * 100
+    print('Accuracy: ', v_accuracy.item())
             
     print('Total time:{:.1f}'.format(time.time() - start_time))
