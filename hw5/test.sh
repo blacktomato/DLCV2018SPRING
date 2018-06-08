3c761f161df91f4fdf2eec4996ee03dafb21b2eb
blacktomato <s90169@gmail.com>
2018-06-09 00:48:48 +0800

    script only for test

---
+++ hw5/test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+bash hw5_p1.sh /data/r06942052/HW5_data/TrimmedVideos/video/valid/ /data/r06942052/HW5_data/TrimmedVideos/label/gt_valid.csv ./
+bash hw5_p2.sh /data/r06942052/HW5_data/TrimmedVideos/video/valid/ /data/r06942052/HW5_data/TrimmedVideos/label/gt_valid.csv ./
+bash hw5_p3.sh /data/r06942052/HW5_data/FullLengthVideos/videos/valid/ ./
